f15da0ee55c9255a7e5a1f4fa53583cea75e64cd
RomaK74 <roma07199@gmail.com>
2022-02-05 00:47:06 +0300

    Add tasks

--- src/components/Item.jsx
+++ src/components/Item.jsx
@@ -6,11 +6,11 @@ import DeleteOutlineIcon from '@mui/icons-material/DeleteOutline';
 import RadioButtonUncheckedIcon from '@mui/icons-material/RadioButtonUnchecked';
 import CheckCircleIcon from '@mui/icons-material/CheckCircle';
 
-export const Item = ({ text }) => {
+export const Item = ({ id, text, completed }) => {
   return (
     <ListItem>
       <div className="d-flex item">
-        <Checkbox icon={<RadioButtonUncheckedIcon />} checkedIcon={<CheckCircleIcon />} />
+        <Checkbox icon={<RadioButtonUncheckedIcon />} checkedIcon={<CheckCircleIcon />} checked={completed} />
         <Typography className="item-text">{text}</Typography>
         <div className="item-buttons d-flex">
           <IconButton>
