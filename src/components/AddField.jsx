79c198408a442504a1a64a8080c7bcd5ca16ba27
RomaK74 <roma07199@gmail.com>
2022-02-05 21:45:08 +0300

    delete task

--- src/components/AddField.jsx
+++ src/components/AddField.jsx
@@ -1,22 +1,41 @@
-import { TextField, Button, Checkbox } from '@mui/material';
+import {TextField, Button, Checkbox} from '@mui/material';
 import AddIcon from '@mui/icons-material/Add';
 import RadioButtonUncheckedIcon from '@mui/icons-material/RadioButtonUnchecked';
 import CheckCircleIcon from '@mui/icons-material/CheckCircle';
+import React from "react";
 
-export const AddField = ({changeTaskComplete, addTask, taskComplete, taskText, changeTaskChange}) => {
-  return (
-    <div className="field">
-      <Checkbox
-        className="checkbox"
-        icon={<RadioButtonUncheckedIcon />}
-        checkedIcon={<CheckCircleIcon />}
-        checked={taskComplete}
-        onClick={changeTaskComplete}
-      />
-      <TextField value={taskText} onChange={(e) => changeTaskChange(e)} placeholder="Введите текст задачи..." variant="standard" fullWidth />
-      <Button onClick={addTask}>
-        <AddIcon />
-      </Button>
-    </div>
-  );
+export const AddField = ({addTask}) => {
+    const [taskText, setTaskText] = React.useState('');
+    const [taskComplete, setTaskComplete] = React.useState(false);
+
+    const changeTaskText = (e) => {
+        setTaskText(e.target.value);
+    }
+
+    const changeTaskComplete = () => {
+        setTaskComplete(!taskComplete);
+    }
+
+    const onClickAdd = () => {
+        addTask(taskText, taskComplete);
+        setTaskComplete(false);
+        setTaskText('');
+    }
+
+    return (
+        <div className="field">
+            <Checkbox
+                className="checkbox"
+                icon={<RadioButtonUncheckedIcon/>}
+                checkedIcon={<CheckCircleIcon/>}
+                checked={taskComplete}
+                onClick={changeTaskComplete}
+            />
+            <TextField value={taskText} onChange={(e) => changeTaskText(e)} placeholder="Введите текст задачи..."
+                       variant="standard" fullWidth/>
+            <Button onClick={onClickAdd}>
+                <AddIcon/>
+            </Button>
+        </div>
+    );
 };
--- src/components/Item.jsx
+++ src/components/Item.jsx
@@ -1,26 +1,31 @@
 import React from 'react';
-import { IconButton, Checkbox, ListItem, Typography } from '@mui/material';
-
+import {IconButton, Checkbox, ListItem, Typography} from '@mui/material';
 import EditIcon from '@mui/icons-material/Edit';
 import DeleteOutlineIcon from '@mui/icons-material/DeleteOutline';
 import RadioButtonUncheckedIcon from '@mui/icons-material/RadioButtonUnchecked';
 import CheckCircleIcon from '@mui/icons-material/CheckCircle';
 
-export const Item = ({ id, text, completed }) => {
-  return (
-    <ListItem>
-      <div className="d-flex item">
-        <Checkbox icon={<RadioButtonUncheckedIcon />} checkedIcon={<CheckCircleIcon />} checked={completed} />
-        <Typography className="item-text">{text}</Typography>
-        <div className="item-buttons d-flex">
-          <IconButton>
-            <EditIcon style={{ fontSize: 20 }} />
-          </IconButton>
-          <IconButton>
-            <DeleteOutlineIcon style={{ fontSize: 20 }} />
-          </IconButton>
-        </div>
-      </div>
-    </ListItem>
-  );
+export const Item = ({id, text, completed, setConfirmOpen, deleteTask}) => {
+    const onClickDelete = (id) => {
+        if (window.confirm('Удалить?')) {
+            deleteTask(id);
+        }
+    }
+
+    return (
+        <ListItem>
+            <div className="d-flex item">
+                <Checkbox icon={<RadioButtonUncheckedIcon/>} checkedIcon={<CheckCircleIcon/>} checked={completed}/>
+                <Typography className="item-text">{text}</Typography>
+                <div className="item-buttons d-flex">
+                    <IconButton>
+                        <EditIcon style={{fontSize: 20}}/>
+                    </IconButton>
+                    <IconButton onClick={() => onClickDelete(id)}>
+                        <DeleteOutlineIcon style={{fontSize: 20}}/>
+                    </IconButton>
+                </div>
+            </div>
+        </ListItem>
+    );
 };
